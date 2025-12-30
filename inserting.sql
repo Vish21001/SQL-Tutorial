65a60d73373263d07b3cbbbbb5c0f5f24019ee12
Vish21001 <agarwalvishisht@gmail.com>
2025-12-30 03:16:21 -0500

    Create inserting.sql

---
+++ inserting.sql
@@ -0,0 +1,5 @@
+INSERT INTO apps VALUES
+(1, 'ChatMaster', 'Social', 500000, 4.5, 0.00),
+(2, 'FitTrack', 'Health', 200000, 4.2, 2.99),
+(3, 'CodeLearn', 'Education', 150000, 4.8, 0.00),
+(4, 'GameZone', 'Games', 800000, 4.1, 1.99);
